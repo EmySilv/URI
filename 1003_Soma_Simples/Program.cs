c46bc1fbda5a70b1197a8254c8f8bcb0c5e4d0d4
Emilly Silva <82464100+EmySilv@users.noreply.github.com>
2022-07-13 15:24:12 -0300

    Add files via upload

---
+++ 1003_Soma_Simples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            c = a + b;
+
+            Console.WriteLine("SOMA = " + c);
+
+            Console.ReadLine();
+        }
+    }
