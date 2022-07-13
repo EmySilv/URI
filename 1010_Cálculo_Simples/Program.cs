3e65d6425790cbd55c5ad68672ca9d9d55141ca2
Emilly Silva <82464100+EmySilv@users.noreply.github.com>
2022-07-13 15:36:53 -0300

    Add files via upload

---
+++ 1010_Cálculo_Simples/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double codigo1, unidade1, preçoUnidade1, codigo2, unidade2, precoUnidade2, total;
+
+            codigo1 = double.Parse(Console.ReadLine());
+
+            unidade1 = double.Parse(Console.ReadLine());
+
+            preçoUnidade1 = double.Parse(Console.ReadLine());
+
+            codigo2 = double.Parse(Console.ReadLine());
+
+            unidade2 = double.Parse(Console.ReadLine());
+
+            precoUnidade2 = double.Parse(Console.ReadLine());
+
+            total = codigo1 + unidade1 + preçoUnidade1 / codigo2 + unidade2 + precoUnidade2;
+
+            Console.WriteLine("VALOR A PAGAR = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
