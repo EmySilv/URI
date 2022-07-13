cc7460b08a950382e82341ff31e2d223b6320d8d
Emilly Silva <82464100+EmySilv@users.noreply.github.com>
2022-07-13 15:13:37 -0300

    Add files via upload

---
+++ 1000_Hello_Word/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1000_Hello_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
