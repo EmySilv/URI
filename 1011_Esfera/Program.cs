4e918269d055e9d2df7e48d3973e608c5ecdffd7
Emilly Silva <82464100+EmySilv@users.noreply.github.com>
2022-07-13 15:37:16 -0300

    Add files via upload

---
+++ 1011_Esfera/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1011_Esfera
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        double valor, pi, resp, r;
+        String quest;
+
+        pi = 3.14159;
+        resp = (4/3.0) * pi * (r * r * r);
+        }
+    }
+}
