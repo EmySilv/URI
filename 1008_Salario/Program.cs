40d09d41e6401f717581f0e724565724f3f67f5a
Emilly Silva <82464100+EmySilv@users.noreply.github.com>
2022-07-13 15:33:18 -0300

    Add files via upload

---
+++ 1008_Salario/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valorHora, quantidadeHora, resultado;
+            int nmr;
+
+            nmr = int.Parse(Console.ReadLine());
+
+            valorHora = double.Parse(Console.ReadLine());
+
+            quantidadeHora = double.Parse(Console.ReadLine());
+
+            resultado = valorHora * quantidadeHora;
+
+            Console.WriteLine("NUMBER = " + nmr);
+
+            Console.WriteLine("SALARY = U$ " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+            
+            Console.ReadLine();
+        }
+    }
