d06299946f8b7d596bac4f9b029d507a03e75dca
Emilly Silva <82464100+EmySilv@users.noreply.github.com>
2022-07-13 15:37:40 -0300

    Add files via upload

---
+++ 1012_Área/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1012_Área
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
