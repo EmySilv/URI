6b67374227b0fadd1cf75d9e1f9575a4692944aa
Emilly Silva <82464100+EmySilv@users.noreply.github.com>
2022-07-13 15:22:35 -0300

    Add files via upload

---
+++ 1002_Área_do_Circulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+
+    static void Main(string[] args) 
+    { 
+
+        double p, r, a;
+        
+        p = 3.14159;
+        r = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+        a = p * (r * r);
+        
+        Console.WriteLine("A=" + a.ToString("F4", CultureInfo.InvariantCulture));
+
+        Console.ReadLine();
+    }
+
+}
