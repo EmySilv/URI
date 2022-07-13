7ef5cd9f3e4a6f7d86408dc1150c2efb72f026d9
Emilly Silva <82464100+EmySilv@users.noreply.github.com>
2022-07-13 15:36:11 -0300

    Add files via upload

---
+++ 1009_Salario_Bônus/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double salarioFixo, vendasEfetuadas, comissao, salarioFinal;
+            string nome;
+        
+            nome = Console.ReadLine();
+
+            salarioFixo = double.Parse(Console.ReadLine());
+
+            vendasEfetuadas = double.Parse(Console.ReadLine());
+
+            comissao = vendasEfetuadas * 0.15;
+
+            salarioFinal = salarioFixo + comissao;
+
+            Console.WriteLine("TOTAL = R$ " + salarioFinal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
